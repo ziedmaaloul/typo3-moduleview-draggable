55f1b7c0a26acd6e6b9e72d775a680900aefe545
Mohamed Zied Maaloul <med.zied.maaloul@gmail.com>
2022-11-01 10:38:46 +0100

    Update DatabaseRecordList.php

--- Classes/DatabaseRecordList.php
+++ Classes/DatabaseRecordList.php
@@ -1,11 +1,26 @@
 <?php
 
 namespace Soft2do\Draggable;
+use \TYPO3\CMS\Core\Imaging\IconFactory;
+use \TYPO3\CMS\Backend\Routing\UriBuilder;
+use \TYPO3\CMS\Backend\Configuration\TranslationConfigurationProvider;
+use \TYPO3\CMS\Core\Utility\GeneralUtility;
+use \TYPO3\CMS\Core\Page\PageRenderer;
+use \Psr\EventDispatcher\EventDispatcherInterface;
 class DatabaseRecordList extends \TYPO3\CMS\Recordlist\RecordList\DatabaseRecordList {
 
-	public function __construct() {
-		parent::__construct();
-		$renderer =\TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Page\PageRenderer::class);
+	protected IconFactory $iconFactory;
+    protected UriBuilder $uriBuilder;
+	protected TranslationConfigurationProvider $translateTools;
+
+
+	public function __construct(IconFactory $iconFactory,
+								UriBuilder $uriBuilder,
+								TranslationConfigurationProvider $translateTools,
+								EventDispatcherInterface $eventDispatcher
+		) {
+		parent::__construct($iconFactory, $uriBuilder,$translateTools,$eventDispatcher);
+		$renderer =GeneralUtility::makeInstance(PageRenderer::class);
 		$renderer->addJsFile('/typo3conf/ext/moduleview_draggable/Resources/Public/JavaScript/RecordListDrag.js', 'text/javascript', false, false, '', true, '|', false, '');
 	}
 }
